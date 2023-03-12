3f73474a57ea8938aa37a946508233b534348fd7
Anthony <bouillant.anthony@gmail.com>
2023-03-12 16:31:30 +0100

    Remove debug console log

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,14 @@
-import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from "./locales/en.json";
+import en from './locales/en.json';
 
 i18n.use(LanguageDetector).init({
   resources: {
     en: { translation: en },
   },
-  fallbackLng: "en",
-  debug: true,
+  fallbackLng: 'en',
+  debug: false,
 });
 
 export default i18n;
