fec3cfd670019d1d36852ffed04a7e591d199805
Anthony <bouillant.anthony@gmail.com>
2023-03-12 16:30:24 +0100

    Refactor auth service

---
+++ src/config/auth.ts
@@ -0,0 +1,89 @@
+import { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/config/firebaseConfig';
+import { FirebaseError } from 'firebase/app';
+import i18n from '@/i18n';
+
+function getGoogleCredentials(): { clientId: string; clientSecret: string } {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  if (!clientId || clientId.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_ID');
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_SECRET');
+  }
+
+  return { clientId, clientSecret };
+}
+
+export const authOptions: NextAuthOptions = {
+  theme: {
+    colorScheme: 'light',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          credentials?.username ?? '',
+          credentials?.password ?? ''
+        )
+          .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            return {
+              id: user.uid,
+              name: user.displayName,
+              email: user.email,
+              image: user.photoURL,
+            };
+          })
+          .catch((error) => {
+            if (
+              error instanceof FirebaseError &&
+              error.code.startsWith('auth/')
+            ) {
+              const { code } = error;
+
+              const translatedMessage = i18n.t(`errorMessages.${code}`);
+              throw new Error(translatedMessage);
+            }
+            throw error;
+          });
+
+        return userCredential;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      if (token) {
+        session.user!.email = token.email;
+      }
+      return session;
+    },
+    redirect() {
+      return '/';
+    },
+  },
+};
